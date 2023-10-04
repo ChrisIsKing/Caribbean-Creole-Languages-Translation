ea484c9cc1a0d7e263de9172f7d28c8e4155650c
Roland Daynauth <daynauth@gmail.com>
2023-10-04 14:13:54 -0400

    fixing stuff around

---
+++ project/project/api/views.py
@@ -0,0 +1,48 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .inference import inference
+from .serializers import EntrySerializer
+from base.models import Entry
+
+
+def translate(text):
+    return inference(
+        key = ""
+        model_name="gpt-3.5-turbo",
+        prompt="Translate the text and provide the resulting Guyanese Creole translation. Please ensure that the translation is clear and accurate. Guyanese Creole is spoken in Guyana and is characterized by its unique vocabulary and grammar. Try to maintain the cultural nuances and colloquialisms if applicable.",
+        prompt_variables={
+            "context": "Text: They are already burnt\n Translation: Dem bon aredii",
+            "instruction": "Translate the following text and provide the resulting Guyanese Creole translation below.",
+            "text": text
+        },
+        max_tokens=2048
+    )
+
+@api_view(["POST"])
+def inferData(request):
+    data = request.data
+
+    if 'text' not in data:
+        return Response("Error: No text provided")
+    
+    text = data['text']
+    translation = translate(text)
+    return Response({'text': text, 'translatedText': translation})
+
+@api_view(["GET"])
+def getEntries(request):
+    entries = Entry.objects.all()
+    serializer = EntrySerializer(entries, many=True)
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def addEntry(request):
+    data = request.data
+
+    serializer = EntrySerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+    else:
+        return Response("Error: Invalid entry")
+    
+    return Response(serializer.data)
