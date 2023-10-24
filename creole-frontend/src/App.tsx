7381eb979c62c2f5fd8ba951f8ef2a22a4fabfc4
Roland Daynauth <daynauth@gmail.com>
2023-10-24 08:58:45 -0400

    removed redundant files

--- creole-frontend/src/App.tsx
+++ creole-frontend/src/App.tsx
@@ -7,7 +7,7 @@ import Bulk from "./Pages/Bulk"
 import Generate from "./Pages/Generate"
 import NavBar from "@/components/NavBar"
 
-import TranslateTest from './components/TranslateTest';
+
 
 
 
@@ -22,7 +22,6 @@ function App() {
                     <Route path="/translate" element={<Main />}></Route>
                     <Route path="/bulk" element={<Bulk></Bulk>}></Route>
                     <Route path="/generate" element={<Generate></Generate>}></Route>
-                    <Route path="/test" element={<TranslateTest></TranslateTest>}></Route>
                 </Routes>
             </Router>
         </div>
